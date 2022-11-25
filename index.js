a012da535ea3dde736bf342f201b5dde728d25bd
Javier Animas <animaz15@gmail.com>
2022-11-25 13:49:29 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+function sendEmail(){
+  Email.send({
+     
+      Host : "smtp.elasticemail.com",
+      Username : "animaz15@gmail.com",
+      Password : "578782789E34AE5380668FE58B28FAE65F48",
+      To : "animaz15@gmail.com",
+      From : "animaz15@gmail.com",
+      Subject : "Nuevo correo en tu portafolio",
+      Body : "Name: " + document.getElementById("name").value
+      + "<br> Email: " + document.getElementById("email").value + "<br> Message: " + document.getElementById("message").value
+      
+  }).then(
+    message => alert("Contact form send succesfully")
+  );
+}
